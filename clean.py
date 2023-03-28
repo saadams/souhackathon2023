7c947c07a156bbc3a055170f12476305187cad2d
Sam <43686234+saadams@users.noreply.github.com>
2023-03-28 14:30:28 -0700

    Create clean.py

---
+++ clean.py
@@ -0,0 +1,9 @@
+def clean_text(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
