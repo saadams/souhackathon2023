c0e32f686ef97c788b226473cc3316ade222097b
Sam <43686234+saadams@users.noreply.github.com>
2023-03-28 13:54:07 -0700

    Add files via upload

--- gen_text.py
+++ gen_text.py
@@ -94,6 +94,22 @@ def graph_model(history,string):
 graph_model(history,'accuracy')
 '''
 
+#produce text
+seed_text = "Thou shall not pass this test"
+next_words = 200
+
+for _ in range(next_words):
+	token_list = tokenizer.texts_to_sequences([seed_text])[0]
+	token_list = pad_sequences([token_list], maxlen=max_seq_len-1, padding='pre')
+	predicted = np.argmax(model.predict(token_list), axis=-1)
+	output_word = ""
+	for word, index in tokenizer.word_index.items():
+		if index == predicted:
+			output_word = word
+			break
+	seed_text += " " + output_word
+print(seed_text)
+
 
 
 
